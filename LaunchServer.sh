bf302de3ee889d8c8e75bc668091a439955f933d
EkoJr <ekojr1337@gmail.com>
2014-04-26 20:10:53 -0700

    Modified core files
    Changed main server files to reduce lag with entities and rendering.

--- LaunchServer.sh
+++ LaunchServer.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Xmx8G -XX:MaxPermSize=256M -jar mcpc-plus-1.6.4-R2.1-forge965-B251.jar nogui
+java -Xms6G -Xmx8G -XX:MaxPermSize=512M -jar mcpc-plus-1.6.4-R2.1-forge965-B251.jar nogui
